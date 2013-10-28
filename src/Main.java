b89c2def1baff40665d2962cc32db3e708f4d193
Eric Chung <chung.yanhon@gmail.com>
2013-10-28 02:56:42 -0700

    added jar files

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,24 @@
+import java.io.IOException;
+
+import org.opengis.geometry.MismatchedDimensionException;
+import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.NoSuchAuthorityCodeException;
+import org.opengis.referencing.operation.TransformException;
+
 
 public class Main {
 
 	/**
 	 * @param args
+	 * @throws TransformException 
+	 * @throws FactoryException 
+	 * @throws IOException 
+	 * @throws NoSuchAuthorityCodeException 
+	 * @throws MismatchedDimensionException 
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws MismatchedDimensionException, NoSuchAuthorityCodeException, IOException, FactoryException, TransformException {
+		Area a = new Area();
+		a.getBuildingsWithinArea();
 	}
 
 }
